05bd21c8e5be6052802ce4276438ee08cd74e683
Игорь Казанова <igor.casanova@gmx.fr>
2026-01-14 23:55:54 +0100

    fix: metadescription by default was undesirable

--- src/pages/+config.ts
+++ src/pages/+config.ts
@@ -5,7 +5,6 @@ import vikeReact from 'vike-react/config';
 import BRAND from '@/config/brand';
 
 export default {
-  description: 'Welcome to the Empire',
   extends: [vikeReact],
   prerender: true,
   title: BRAND
