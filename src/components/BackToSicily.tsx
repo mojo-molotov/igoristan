825121a046a97b2509ca59ac7ee08ecdfb83c87e
Игорь Казанова <igor.casanova@gmx.fr>
2026-01-16 22:52:12 +0100

    feat: randomized back to sicily component

--- src/components/BackToSicily.tsx
+++ src/components/BackToSicily.tsx
@@ -1,9 +1,47 @@
+import { useEffect, useState } from 'react';
+
+import type { UnionToTuple } from '@/types/utils';
+
+import { pickRandom } from '@/lib/pickRandom';
+
 import Link from './Link';
 
-const BackToSicily = () => (
-  <Link className="mx-auto w-fit text-xl no-underline" href="http://bestofsicily.com">
-    ✈️ Go back to Sicilia
-  </Link>
-);
+const SICILY_CONTENT = {
+  'http://bestofsicily.com': [
+    '✈️ Go back to Sicilia',
+    '✈️ Go back to your 30 centuries of history',
+    '✈️ Go taste the Sicilian wine renaissance at home',
+    "✈️ Go obey Frederick II's imperial command",
+    '✈️ Go admire your Baroque palaces in Sicilia'
+  ],
+  'http://www.peoplesofsicily.com/women.htm': [
+    "✈️ Go read Jacqueline Alio's 224 pages - far away",
+    '✈️ Go meet the Saints, Queens and Rebels - in Sicilia',
+    '✈️ Go join the sisterhood of conviction - somewhere else'
+  ],
+  'http://www.jacquelinealio.com/margaret.htm': ['✈️ Go visit Monreale Abbey where you belong', "✈️ Go seek Thomas Becket's blessing in Sicilia"]
+} as const;
+
+type SicilyWebsite = UnionToTuple<keyof typeof SICILY_CONTENT>;
+const SICILY_WEBSITES = Object.freeze(Object.keys(SICILY_CONTENT)) as SicilyWebsite;
+
+const BackToSicily = () => {
+  const [sicilyWebsite, setSicilyWebsite] = useState<string>(SICILY_WEBSITES[0]);
+  const [goAwayMsg, setGoAwayMsg] = useState<string>(SICILY_CONTENT[SICILY_WEBSITES[0]][0]);
+
+  useEffect(() => {
+    const website = pickRandom(SICILY_WEBSITES);
+    const msg = pickRandom(SICILY_CONTENT[website]);
+
+    setSicilyWebsite(website);
+    setGoAwayMsg(msg);
+  }, []);
+
+  return (
+    <Link className="mx-auto w-fit animate-fade-in text-xl no-underline" href={sicilyWebsite}>
+      {goAwayMsg}
+    </Link>
+  );
+};
 
 export default BackToSicily;
--- src/types/utils.ts
+++ src/types/utils.ts
@@ -1,1 +1,7 @@
+export type UnionToTuple<T> = (
+  (T extends any ? (t: T) => T : never) extends infer U ? ((U extends any ? (u: U) => any : never) extends (v: infer V) => any ? V : never) : never
+) extends (_: any) => infer W
+  ? readonly [...UnionToTuple<Exclude<T, W>>, W]
+  : readonly [];
+
 export type NonEmptyArray<T> = readonly [T, ...T[]];
